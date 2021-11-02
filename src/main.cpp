0463d8ed4b61fc4c1036ab62295f158c54901232
mdeboute <martin.deboute@gmail.com>
2021-11-02 18:26:01 +0100

    create a new method

--- src/main.cpp
+++ src/main.cpp
@@ -11,17 +11,13 @@ int main()
         // (x, y, w) -> edge from x to y with weight w
         {0,1,2},{0,2,4},{1,4,3},{2,3,2},{3,1,4},{4,3,3}
     };
-    int N = 6; // Number of vertices in the graph
+    // number of vertices in the graph
+    int N = 5;
     // calculate number of edges
     int n = sizeof(edges)/sizeof(edges[0]);
     // construct graph
     Graph Graph(edges, n, N);
     // print adjacency list representation of graph
-    cout<<"Graph adjacency list "<<endl<<"(start_vertex, end_vertex, weight):"<<endl;
-    for (int i = 0; i < N; i++)
-    {
-        // display adjacent vertices of vertex i
-        display_AdjList(Graph.head[i], i);
-    }
+    Graph.printGraph();
     return 0;
 }
