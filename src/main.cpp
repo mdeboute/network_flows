04dbe86e9e66eadf96321258ccf84d270ad1e124
mdeboute <martin.deboute@gmail.com>
2022-02-06 18:57:27 +0100

    test

--- src/main.cpp
+++ src/main.cpp
@@ -7,11 +7,11 @@ int main()
 {
   Graph graph = mincost::parse("../data/mincost/test.txt");
   graph.print();
-  cout << endl;
-  cout << "Residual graph:\n"
-       << endl;
-  Graph *rGraph = graph.getResidualGraph();
-  rGraph->print();
+  // cout << endl;
+  // cout << "Residual graph:\n"
+  //      << endl;
+  // Graph *rGraph = graph.getResidualGraph(); //undefined reference to `Graph::getResidualGraph()' ?
+  // rGraph->print();
 
   cout << endl;
   Graph graph2 = maxflow::parse("../data/maxflow/instance_1.max");
