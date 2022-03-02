f8d7729783625ace1afd32aece15b09e671151a0
grosbg <basile.blayac@etu.u-bordeaux.fr>
2022-03-02 15:01:06 +0100

    add instances mincost + main refactor

--- src/exec/main.cpp
+++ src/exec/main.cpp
@@ -11,11 +11,14 @@ using namespace std;
 
 int main()
 {
-  maxFlowComparison("../data/maxflow/instance_10_20_39.max", "instance_10_20_39.max", true);
+  // maxFlowComparison("../data/maxflow/instance_10_20_39.max", "instance_10_20_39.max", true);
   // maxFlowBenchmarks(true);
 
-  // Graph graphMin = minCost::parse("../data/mincost/simple_min_cost.min");
-  // cycleCancelling(&graphMin);
+  Graph graph = minCost::parse("../data/mincost/small_instances/instance_10_20_1.max.min");
+  PL::minCost(graph, true);
+
+  Graph graphMin = minCost::parse("../data/mincost/small_instances/instance_10_20_1.max.min");
+  cycleCancelling(&graphMin);
 
   return 0;
 }
