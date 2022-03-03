1e7ba74641b2f4d8a7db89ee0f2bbfe95e27c116
Pierre Pinet <pipinet@emi.u-bordeaux.fr>
2022-03-03 13:17:19 +0100

    Script pyhton pour générer des instances coût min.

---
+++ generator/mincost/setRandomCosts.py
@@ -0,0 +1,21 @@
+from random import randint
+
+
+def setRandomCosts():
+    fileName = input()
+
+    file= open(fileName,"r")
+    lines= file.readlines()
+    file.close()
+
+    nbLines = int(lines[1].split()[3])
+
+    for index in range(nbLines):
+        lines[3+index] = lines[3+index][0:len(lines[3+index])-2] + str(randint(0,100)) + "\n"
+
+    file= open(fileName,"w")
+    file.writelines(lines)
+    file.close()
+
+
+setRandomCosts()
--- src/exec/main.cpp
+++ src/exec/main.cpp
@@ -11,14 +11,8 @@ using namespace std;
 
 int main()
 {
-  // maxFlowComparison("../data/maxflow/instance_10_20_39.max", "instance_10_20_39.max", true);
+  maxFlowComparison("../data/maxflow/instance_10_20_567.max", "instance_10_20_567.max", true);
   // maxFlowBenchmarks(true);
-
-  Graph graph = minCost::parse("../data/mincost/small_instances/instance_10_20_1.max.min");
-  PL::minCost(graph, true);
-
-  Graph graphMin = minCost::parse("../data/mincost/small_instances/instance_10_20_1.max.min");
-  cycleCancelling(&graphMin);
 
   return 0;
 }
